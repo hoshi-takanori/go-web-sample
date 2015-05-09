bd5364bdf29e89a4d38724ec856e261b39041382
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2015-05-09 12:41:13 +0900

    add config.json

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
+	"io/ioutil"
 	"net/http"
 )
 
+type Config struct {
+	Address string
+}
+
+var config Config
+
 func main() {
+	str, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(str, &config)
+	if err != nil {
+		panic(err)
+	}
+
 	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(config.Address, nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
