9a051d3fa579c24f146c6cda975253d4a4987103
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2015-05-10 16:33:21 +0900

    add staticDir

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/yosssi/ace"
 )
 
 type Config struct {
 	Address string
+	StaticDir string
 
 	AceOptions ace.Options
 }
@@ -26,10 +28,28 @@ func main() {
 		panic(err)
 	}
 
+	if config.StaticDir != "" {
+		staticDir(config.StaticDir, http.DefaultServeMux)
+	}
+
 	http.HandleFunc("/", hello)
 	err = http.ListenAndServe(config.Address, nil)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func staticDir(dirname string, mux *http.ServeMux) {
+	fis, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		panic(err)
+	}
+
+	fileServer := http.FileServer(http.Dir(dirname))
+	for _, fi := range fis {
+		if fi.IsDir() && !strings.HasPrefix(fi.Name(), ".") {
+			mux.Handle("/" + fi.Name() + "/", fileServer)
+		}
 	}
 }
 
