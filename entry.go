ad0b29f97c10e5197660f5f45c85c32c05f46ef3
Hoshi Takanori <hoshi-takanori@users.noreply.github.com>
2015-06-24 11:57:02 +0900

    add entry.go

---
+++ entry.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"os"
+	"path"
+	"sort"
+	"strconv"
+	"time"
+)
+
+type Section struct {
+	Name    string
+	Entries []Entry
+}
+
+type Entry struct {
+	Name string
+	Path string
+	Date string
+	Dcls string
+}
+
+type UserEntry struct {
+	user  User
+	path  string
+	mtime time.Time
+}
+
+func NewYearlyEntry(ue UserEntry, today time.Time) Entry {
+	date := "-"
+	dcls := "date-none"
+	if !ue.mtime.IsZero() {
+		date = ue.mtime.Format("2006/01/02 15:04:05")
+		dcls = DateClass(ue.mtime, today)
+	}
+	return Entry{ue.user.name, ue.path, date, dcls}
+}
+
+func MakeYearlySections(list []UserEntry, targetYear int) ([]Section, error) {
+	now := time.Now()
+	year, month, day := now.Date()
+	today := time.Date(year, month, day, 0, 0, 0, 0, now.Location())
+
+	years := []int{}
+	entries := map[int][]Entry{}
+	for _, ue := range list {
+		year := ue.user.year
+		if targetYear != 0 && year != targetYear {
+			year = 0
+		}
+
+		list, ok := entries[year]
+		if ok {
+			entries[year] = append(list, NewYearlyEntry(ue, today))
+		} else {
+			entries[year] = []Entry{NewYearlyEntry(ue, today)}
+			years = append(years, year)
+		}
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(years)))
+
+	sections := []Section{}
+	for _, year := range years {
+		name := "Staff"
+		if year != 0 {
+			name = "Fresh"
+			if targetYear == 0 {
+				name += " " + strconv.Itoa(year)
+			}
+		}
+		sections = append(sections, Section{name, entries[year]})
+	}
+
+	return sections, nil
+}
+
+func ListFiles(users []User) []UserEntry {
+	list := []UserEntry{}
+	for _, user := range users {
+		ue := UserEntry{user: user}
+
+		dir := "staff"
+		if user.staffYear == 0 && user.year != 0 {
+			dir = strconv.Itoa(user.year)
+		}
+		file := path.Join(dir, user.name, "diary.html")
+
+		fi, err := os.Stat(path.Join(config.PrivateDir, file))
+		if err == nil && fi.Mode().IsRegular() {
+			ue.path = file
+			ue.mtime = fi.ModTime()
+		}
+
+		list = append(list, ue)
+	}
+
+	return list
+}
+
+func DateClass(date, today time.Time) string {
+	diff := today.Sub(date)
+	day := 24 * time.Hour
+	if diff <= 0 {
+		return "date-today"
+	} else if diff <= day {
+		return "date-yesterday"
+	} else if diff <= 3*day {
+		return "date-recent"
+	} else if diff <= 7*day {
+		return "date-week"
+	} else if diff <= 30*day {
+		return "date-month"
+	} else {
+		return "date-old"
+	}
+}
--- hello.go
+++ hello.go
@@ -2,29 +2,7 @@ package main
 
 import (
 	"net/http"
-	"os"
-	"path"
-	"sort"
-	"strconv"
-	"time"
 )
-
-type Section struct {
-	Name    string
-	Entries []Entry
-}
-
-type Entry struct {
-	Name string
-	Path string
-	Date string
-	Dcls string
-}
-
-type UserEntry struct {
-	user  User
-	entry Entry
-}
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	data := NewData(config.Title)
@@ -59,85 +37,4 @@ func MakeSections(username string) ([]Section, error) {
 
 	list := ListFiles(users)
 	return MakeYearlySections(list, year)
-}
-
-func MakeYearlySections(list []UserEntry, targetYear int) ([]Section, error) {
-	years := []int{}
-	entries := map[int][]Entry{}
-	for _, ue := range list {
-		year := ue.user.year
-		if targetYear != 0 && year != targetYear {
-			year = 0
-		}
-
-		list, ok := entries[year]
-		if ok {
-			entries[year] = append(list, ue.entry)
-		} else {
-			entries[year] = []Entry{ue.entry}
-			years = append(years, year)
-		}
-	}
-
-	sort.Sort(sort.Reverse(sort.IntSlice(years)))
-
-	sections := []Section{}
-	for _, year := range years {
-		name := "Staff"
-		if year != 0 {
-			name = "Fresh"
-			if targetYear == 0 {
-				name += " " + strconv.Itoa(year)
-			}
-		}
-		sections = append(sections, Section{name, entries[year]})
-	}
-
-	return sections, nil
-}
-
-func ListFiles(users []User) []UserEntry {
-	now := time.Now()
-	year, month, day := now.Date()
-	today := time.Date(year, month, day, 0, 0, 0, 0, now.Location())
-
-	list := []UserEntry{}
-	for _, user := range users {
-		dir := "staff"
-		if user.staffYear == 0 && user.year != 0 {
-			dir = strconv.Itoa(user.year)
-		}
-		file := path.Join(dir, user.name, "diary.html")
-
-		fi, err := os.Stat(path.Join(config.PrivateDir, file))
-		date := "-"
-		dcls := "date-none"
-		if err == nil && fi.Mode().IsRegular() {
-			date = fi.ModTime().Format("2006/01/02 15:04:05")
-			dcls = DateClass(fi.ModTime(), today)
-		} else {
-			file = ""
-		}
-
-		list = append(list, UserEntry{user, Entry{user.name, file, date, dcls}})
-	}
-	return list
-}
-
-func DateClass(date, today time.Time) string {
-	diff := today.Sub(date)
-	day := 24 * time.Hour
-	if diff <= 0 {
-		return "date-today"
-	} else if diff <= day {
-		return "date-yesterday"
-	} else if diff <= 3*day {
-		return "date-recent"
-	} else if diff <= 7*day {
-		return "date-week"
-	} else if diff <= 30*day {
-		return "date-month"
-	} else {
-		return "date-old"
-	}
 }
